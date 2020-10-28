711521353915676e35c5070325a3df84e8314af4
f-sky <939949352@qq.com>
2020-10-28 15:20:20 +0800

    first commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,47 @@
 #include <pybind11/pybind11.h>
+#include "ceres/ceres.h"
+#include "glog/logging.h"
+using ceres::AutoDiffCostFunction;
+using ceres::CostFunction;
+using ceres::Problem;
+using ceres::Solve;
+using ceres::Solver;
+
+struct CostFunctor {
+  template <typename T>
+  bool operator()(const T* const x, T* residual) const {
+    residual[0] = 12.0 - x[0];
+    return true;
+  }
+};
 
 int add(int i, int j) {
     return i + j;
 }
-
+void ceres_test(){
+    double x = 0.5;
+    const double initial_x = x;
+    // Build the problem.
+    Problem problem;
+    // Set up the only cost function (also known as residual). This uses
+    // auto-differentiation to obtain the derivative (jacobian).
+    CostFunction* cost_function =
+        new AutoDiffCostFunction<CostFunctor, 1, 1>(new CostFunctor);
+    problem.AddResidualBlock(cost_function, nullptr, &x);
+    // Run the solver!
+    Solver::Options options;
+    options.minimizer_progress_to_stdout = true;
+    Solver::Summary summary;
+    Solve(options, &problem, &summary);
+    std::cout << summary.BriefReport() << "\n";
+    std::cout << "x : " << initial_x << " -> " << x << "\n";
+}
 namespace py = pybind11;
 
 PYBIND11_MODULE(cmake_example, m) {
-    m.doc() = R"pbdoc(
-        Pybind11 example plugin
-        -----------------------
-
-        .. currentmodule:: cmake_example
-
-        .. autosummary::
-           :toctree: _generate
-
-           add
-           subtract
-    )pbdoc";
-
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
-
-        Some other explanation about the add function.
-    )pbdoc");
-
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-        Subtract two numbers
-
-        Some other explanation about the subtract function.
-    )pbdoc");
+   
+    m.def("add", &add);
+    m.def("ceres_test", &ceres_test);
 
 #ifdef VERSION_INFO
     m.attr("__version__") = VERSION_INFO;
